3ab918e415b2483040fc167135b99954be029e57
Fredy2024 <fredobonyo5@gmail.com>
2024-03-05 08:37:14 +0300

    week2 py assignmt

---
+++ user_input (1).py
@@ -0,0 +1,9 @@
+name = input(" waht is your name?")
+age = input("what is your age?")
+location = input("what is your location?")
+print("your name is "+ "name" + "your age is "+ "age" + "and your location is" + "location")
+      
+
+
+
+
